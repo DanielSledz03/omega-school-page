6c0ed0b52ceca7af2fbd181632a32905e4b4682c
DanielSledz03 <daniel.sledz@pluscode.io>
2023-02-11 03:49:17 +0100

    test3

--- data/galleryData.ts
+++ data/galleryData.ts
@@ -59,27 +59,27 @@ import ChristmasMarket_5 from '../public/assets/gallery/Kiermasz Świąteczny/5.
 import ChristmasMarket_8 from '../public/assets/gallery/Kiermasz Świąteczny/8.jpg'
 import ChristmasMarket_9 from '../public/assets/gallery/Kiermasz Świąteczny/9.jpg'
 import ChristmasMarket_10 from '../public/assets/gallery/Kiermasz Świąteczny/10.jpg'
-import ChristmasMarket_11 from '../public/assets/gallery/Kiermasz Świąteczny/11.jpg'
-import ChristmasMarket_12 from '../public/assets/gallery/Kiermasz Świąteczny/12.jpg'
-import ChristmasMarket_13 from '../public/assets/gallery/Kiermasz Świąteczny/13.jpg'
-import ChristmasMarket_14 from '../public/assets/gallery/Kiermasz Świąteczny/14.jpg'
-import ChristmasMarket_15 from '../public/assets/gallery/Kiermasz Świąteczny/15.jpg'
-import ChristmasMarket_16 from '../public/assets/gallery/Kiermasz Świąteczny/16.jpg'
-import ChristmasMarket_17 from '../public/assets/gallery/Kiermasz Świąteczny/17.jpg'
-import ChristmasMarket_18 from '../public/assets/gallery/Kiermasz Świąteczny/18.jpg'
+// import ChristmasMarket_11 from '../public/assets/gallery/Kiermasz Świąteczny/11.jpg'
+// import ChristmasMarket_12 from '../public/assets/gallery/Kiermasz Świąteczny/12.jpg'
+// import ChristmasMarket_13 from '../public/assets/gallery/Kiermasz Świąteczny/13.jpg'
+// import ChristmasMarket_14 from '../public/assets/gallery/Kiermasz Świąteczny/14.jpg'
+// import ChristmasMarket_15 from '../public/assets/gallery/Kiermasz Świąteczny/15.jpg'
+// import ChristmasMarket_16 from '../public/assets/gallery/Kiermasz Świąteczny/16.jpg'
+// import ChristmasMarket_17 from '../public/assets/gallery/Kiermasz Świąteczny/17.jpg'
+// import ChristmasMarket_18 from '../public/assets/gallery/Kiermasz Świąteczny/18.jpg'
 
-import NativityPlay_1 from '../public/assets/gallery/Jasełka/1.jpg'
-import NativityPlay_2 from '../public/assets/gallery/Jasełka/2.jpg'
-import NativityPlay_3 from '../public/assets/gallery/Jasełka/3.jpg'
+// import NativityPlay_1 from '../public/assets/gallery/Jasełka/1.jpg'
+// import NativityPlay_2 from '../public/assets/gallery/Jasełka/2.jpg'
+// import NativityPlay_3 from '../public/assets/gallery/Jasełka/3.jpg'
 import NativityPlay_4 from '../public/assets/gallery/Jasełka/4.jpg'
 import NativityPlay_5 from '../public/assets/gallery/Jasełka/5.jpg'
 import NativityPlay_6 from '../public/assets/gallery/Jasełka/6.jpg'
 import NativityPlay_7 from '../public/assets/gallery/Jasełka/7.jpg'
 import NativityPlay_8 from '../public/assets/gallery/Jasełka/8.jpg'
-import NativityPlay_9 from '../public/assets/gallery/Jasełka/9.jpg'
-import NativityPlay_10 from '../public/assets/gallery/Jasełka/10.jpg'
-import NativityPlay_11 from '../public/assets/gallery/Jasełka/11.jpg'
-import NativityPlay_12 from '../public/assets/gallery/Jasełka/12.jpg'
+// import NativityPlay_9 from '../public/assets/gallery/Jasełka/9.jpg'
+// import NativityPlay_10 from '../public/assets/gallery/Jasełka/10.jpg'
+// import NativityPlay_11 from '../public/assets/gallery/Jasełka/11.jpg'
+// import NativityPlay_12 from '../public/assets/gallery/Jasełka/12.jpg'
 
 export const GalleryData = [
   {
@@ -171,33 +171,33 @@ export const GalleryData = [
       ChristmasMarket_8,
       ChristmasMarket_9,
       ChristmasMarket_10,
-      ChristmasMarket_11,
-      ChristmasMarket_12,
-      ChristmasMarket_13,
-      ChristmasMarket_14,
-      ChristmasMarket_15,
-      ChristmasMarket_16,
-      ChristmasMarket_17,
-      ChristmasMarket_18,
+      // ChristmasMarket_11,
+      // ChristmasMarket_12,
+      // ChristmasMarket_13,
+      // ChristmasMarket_14,
+      // ChristmasMarket_15,
+      // ChristmasMarket_16,
+      // ChristmasMarket_17,
+      // ChristmasMarket_18,
     ],
-    thumbnail: ChristmasMarket_14,
+    thumbnail: ChristmasMarket_10,
   },
   {
     header: 'Jasełka',
     images: [
-      NativityPlay_1,
-      NativityPlay_2,
-      NativityPlay_3,
+      // NativityPlay_1,
+      // NativityPlay_2,
+      // NativityPlay_3,
       NativityPlay_4,
       NativityPlay_5,
       NativityPlay_6,
       NativityPlay_7,
       NativityPlay_8,
-      NativityPlay_9,
-      NativityPlay_10,
-      NativityPlay_11,
-      NativityPlay_12,
+      // NativityPlay_9,
+      // NativityPlay_10,
+      // NativityPlay_11,
+      // NativityPlay_12,
     ],
-    thumbnail: NativityPlay_12,
+    thumbnail: NativityPlay_7,
   },
 ]
